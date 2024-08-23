f41d7de0a416a334a074d66ff1097d472d0c3221
John Doe <hashim104243@gmail.com>
2024-08-23 18:42:56 +0500

    commit service

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,10 +5,14 @@ export default function Services() {
     <div className="bg-[#25262F] p-3 sm:p-16 font-sans  ">
       <div className="flex flex-col items-center pt-12">
         <div className="text-[#80DB66] tracking-[2px] text-xl">Service</div>
-        <div className="text-white font-bold text-4xl p-6">What I offer</div>
+        <div className="text-white font-medium text-4xl p-6">What I offer</div>
       </div>
 
-      <div className="md:grid md:grid-cols-2 md:mr-9 place-content-center md:place-content-center lg:place-content-center  md:gap-x-[80px]  md:gap-y-[0px] lg:gap-x-[90px] lg:ml-[-10px]  xl:gap-x-[100px] 2xl:ml-[100px] 2xl:place-content-center-center">
+      <div
+        className="md:grid md:grid-cols-2 md:mr-9 place-content-center  lg:place-content-center  md:gap-x-[80px]  md:gap-y-[0px] lg:gap-x-[90px]   xl:gap-x-[77px]
+      
+      2xl:ml-[100px]  2xl:gap-x-[0px] 2xl:place-content-center-center md:ml-[-17px] "
+      >
         <div className="bg-white rounded-[10px] p-7    mb-3 mt-7  md:w-[330px] md:pb-[60px] md:pt-[60px] lg:w-[450px] lg:p-8 lg:flex xl:w-[570px]   ">
           <div className="p-3    lg:w-28 ">
             <img src="ser-icon1.png" className="w-12 md:w-[73px]      " />
@@ -59,7 +63,7 @@ export default function Services() {
             <img src="ser-icon4.png" className="w-12 md:w-[73px]      " />
           </div>
           <div className="lg:w-52">
-            <div className="font-bold text-[26px] sm:text-[28px] mt-3 mb-3 lg:w-[300px] lg:text-[30px]">
+            <div className="font-medium text-[26px] sm:text-[28px] mt-3 mb-3 lg:w-[300px] lg:text-[30px]">
               Web Development
             </div>
             <div className="text-[19px] text-[#4d4d5c]  sm:w-[440px] sm:text-[20px] md:w-[270px]  lg:w-[290px] lg:text-[20px] xl:w-[400px] ">
