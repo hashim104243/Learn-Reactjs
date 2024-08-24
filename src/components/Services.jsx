54207c350275373c4cb88a32227fe56250e57958
John Doe <hashim104243@gmail.com>
2024-08-24 11:07:05 +0500

    commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+// md:gap-x-[80px]  md:gap-y-[15px] lg:gap-x-[90px]   xl:gap-x-[77px] 2xl:ml-[100px]  2xl:gap-x-[0px]
 export default function Services() {
   return (
     <div className="bg-[#25262F] p-3 sm:p-16 font-sans  ">
@@ -8,8 +8,8 @@ export default function Services() {
         <div className="text-white font-medium text-4xl p-6">What I offer</div>
       </div>
 
-      <div className="md:grid md:grid-cols-2 md:mr-9 place-content-center  lg:place-content-center  md:gap-x-[80px]  md:gap-y-[0px] lg:gap-x-[90px]   xl:gap-x-[77px] 2xl:ml-[100px]  2xl:gap-x-[0px] 2xl:place-content-center-center md:ml-[-17px] ">
-        <div className="bg-white rounded-[10px] p-7    mb-3 mt-7  md:w-[330px] md:pb-[60px] md:pt-[60px] lg:w-[450px] lg:p-8 lg:flex xl:w-[570px] 2xl:ml-[22px]   ">
+      <div className="md:grid md:grid-cols-2 mx-auto gap-x-[20px]  place-items-center   md:ml-[-17px]  ">
+        <div className="bg-white rounded-[10px] p-7 mx-auto    mb-3 mt-7   md:pb-[60px] md:pt-[60px] lg:w-[450px] lg:p-8 lg:flex xl:w-[570px] 2xl:ml-[22px]  md:w-full ">
           <div className="p-3    lg:w-28 ">
             <img src="ser-icon1.png" className="w-12 md:w-[73px]      " />
           </div>
@@ -17,14 +17,14 @@ export default function Services() {
             <div className="font-medium text-[26px] sm:text-[28px] mt-3 mb-3 lg:w-[300px] lg:text-[30px]">
               Web Development
             </div>
-            <div className="text-[19px] text-[#4d4d5c]  sm:w-[440px] sm:text-[20px] md:w-[270px]  lg:w-[290px] lg:text-[20px] xl:w-[400px] ">
+            <div className="text-[19px] text-[#4d4d5c]   sm:text-[20px]    lg:text-[20px] xl:w-[400px] ">
               Ludantium totam rem aperia meaququae ab tatis et quasi architecto
               beatae vit dunt ut labore et dolore magna aliqua.
             </div>
           </div>
         </div>
 
-        <div className="bg-white rounded-[10px] p-7    mb-3 mt-7  md:w-[330px] md:pb-[60px] md:pt-[60px] lg:w-[450px] lg:p-8 lg:flex xl:w-[570px]   ">
+        <div className="bg-white rounded-[10px] p-7  mx-auto     mb-3 mt-7   md:pb-[60px] md:pt-[60px] lg:w-[450px] lg:p-8 lg:flex xl:w-[570px] md:w-full   ">
           <div className="p-3    lg:w-28 ">
             <img src="ser-icon2.png" className="w-12 md:w-[73px]      " />
           </div>
@@ -32,14 +32,14 @@ export default function Services() {
             <div className="font-medium text-[26px] sm:text-[28px] mt-3 mb-3 lg:w-[300px] lg:text-[30px]">
               Web Development
             </div>
-            <div className="text-[19px] text-[#4d4d5c]  sm:w-[440px] sm:text-[20px] md:w-[270px]  lg:w-[290px] lg:text-[20px] xl:w-[400px] ">
+            <div className="text-[19px] text-[#4d4d5c]   sm:text-[20px]  lg:text-[20px] xl:w-[400px] ">
               Ludantium totam rem aperia meaququae ab tatis et quasi architecto
               beatae vit dunt ut labore et dolore magna aliqua.
             </div>
           </div>
         </div>
 
-        <div className="bg-white rounded-[10px] p-7    mb-3 mt-7  md:w-[330px] md:pb-[60px] md:pt-[60px] lg:w-[450px] lg:p-8 lg:flex xl:w-[570px] 2xl:ml-[24px]  md:mt-[10px] lg:mt-[24px] 2xl:mt-[32px]">
+        <div className="bg-white rounded-[10px] p-7  mx-auto    mb-3 mt-7 md:pb-[60px] md:pt-[60px] lg:w-[450px] lg:p-8 lg:flex xl:w-[570px] 2xl:ml-[24px]  md:mt-[10px] lg:mt-[24px] 2xl:mt-[32px] md:w-full   ">
           <div className="p-3    lg:w-28 ">
             <img src="ser-icon3.png" className="w-12 md:w-[73px]      " />
           </div>
@@ -47,14 +47,14 @@ export default function Services() {
             <div className="font-medium text-[26px] sm:text-[28px] mt-3 mb-3 lg:w-[300px] lg:text-[30px]">
               Web Development
             </div>
-            <div className="text-[19px] text-[#4d4d5c]  sm:w-[440px] sm:text-[20px] md:w-[270px]  lg:w-[290px] lg:text-[20px] xl:w-[400px] ">
+            <div className="text-[19px] text-[#4d4d5c]   sm:text-[20px]   lg:text-[20px] xl:w-[400px] ">
               Ludantium totam rem aperia meaququae ab tatis et quasi architecto
               beatae vit dunt ut labore et dolore magna aliqua.
             </div>
           </div>
         </div>
 
-        <div className="bg-white rounded-[10px] p-7    mb-3 mt-7  md:w-[330px] md:pb-[60px] md:pt-[60px] lg:w-[450px] lg:p-8 lg:flex xl:w-[570px]    md:mt-[10px] lg:mt-[24px] 2xl:mt-[32px]">
+        <div className="bg-white rounded-[10px] p-7 mx-auto  md:w-full   mb-3 mt-7   md:pb-[60px] md:pt-[60px] lg:w-[450px] lg:p-8 lg:flex xl:w-[570px]    md:mt-[10px] lg:mt-[24px] 2xl:mt-[32px] ">
           <div className="p-3    lg:w-28 ">
             <img src="ser-icon4.png" className="w-12 md:w-[73px]      " />
           </div>
@@ -62,7 +62,7 @@ export default function Services() {
             <div className="font-medium text-[26px] sm:text-[28px] mt-3 mb-3 lg:w-[300px]  lg:text-[30px]">
               Web Development
             </div>
-            <div className="text-[19px] text-[#4d4d5c]  sm:w-[440px] sm:text-[20px] md:w-[270px]  lg:w-[290px] lg:text-[20px] xl:w-[400px] ">
+            <div className="text-[19px] text-[#4d4d5c]   sm:text-[20px]   lg:text-[20px] xl:w-[400px] ">
               Ludantium totam rem aperia meaququae ab tatis et quasi architecto
               beatae vit dunt ut labore et dolore magna aliqua.
             </div>
