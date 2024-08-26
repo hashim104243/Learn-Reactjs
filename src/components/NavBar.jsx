7feb6d4f898e6e9425779915cdf9482132b12df8
John Doe <hashim104243@gmail.com>
2024-08-26 16:59:53 +0500

    commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { list } from "./NavBarItems";
+import { HiBars3 } from "react-icons/hi2";
 export default function NavBar() {
   return (
     <nav className="  flex items-center p-2   justify-around 2xl:justify-around w-full  fixed top-0    bg-[#292522]  lg:pt-7 z-40 font-sans mx-auto  ">
@@ -27,7 +28,9 @@ export default function NavBar() {
         <button className="border border-solid border-[#80DB66] p-3 lg:w-40 lg:text-[16px] font-medium text-xs rounded hover:bg-[#80DB66] hover:text-white  cursor-pointer">
           DOWNLOAD CV
         </button>
-        <div className="text-2xl text-[#80DB66] md:text-3xl md:pl-[23px]   lg:hidden"></div>
+        <div className="text-2xl text-[#80DB66] md:text-3xl md:pl-[23px]   lg:hidden">
+          <HiBars3 />
+        </div>
       </div>
     </nav>
   );
